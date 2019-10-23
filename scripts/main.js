a6e91ac428ba5886d62c6ce06ff4bdc4af7793ea
e502104 <sven.jaun@sbb.ch>
2019-10-23 09:00:30 +0200

    Added Nav features, only few :bug: not fixed

--- scripts/main.js
+++ scripts/main.js
@@ -1,91 +1,190 @@
 var nails = new Nails({
     el: "body", //Start with # to specify id
     data: {
-      title: "0xEB0000",
+        title: "0xEB0000",
     },
     methods: {
-      onInit() {
-      },
-      onMounted(currentState) {
-        currentState.data.photoInfos = [{
-          "name": "Dominic Jaermann",
-          "title": "Briefkasten",
-          "text": "Ein Briefkasten eines einheimischen",
-          "ISO": "100",
-          "Brennweite": "4.8mm",
-          "Blende": "f/3.2",
-          "Verschlusszeit": "1/100 Sek."
-      },
-      {
-          "name": "Dominic Jaermann",
-          "title": "Grill",
-          "text": "Text zu Grill",
-          "ISO": "100",
-          "Brennweite": "11.5mm",
-          "Blende": "f\/3.4",
-          "Verschlusszeit": "1/50 Sek."
-      },
-      {
-          "name": "Dominic Jaermann",
-          "title": "Monolith",
-          "text": "Ein Monolith",
-          "ISO": "100",
-          "Brennweite": "4.8mm",
-          "Blende": "f\/2.8",
-          "Verschlusszeit": "1/30 Sek."
-      },
-      {
-          "name": "Mladen Brankovic",
-          "title": "Giraffe",
-          "text": "Eine Giraffe",
-          "ISO": "100",
-          "Brennweite": "4.8mm",
-          "Blende": "f\/2.8",
-          "Verschlusszeit": "1/80 Sek."
-      },
-      {
-          "name": "Mladen Brankovic",
-          "title": "Skulptur",
-          "text": "Eine Skulptur",
-          "ISO": "100",
-          "Brennweite": "4.8mm",
-          "Blende": "f\/4.0",
-          "Verschlusszeit": "1/200 Sek."
-      },
-      {
-          "name": "Mladen Brankovic",
-          "title": "Verkehrshut",
-          "text": "Ein Verkehrshut",
-          "ISO": "100",
-          "Brennweite": "8.5mm",
-          "Blende": "f\/3.2",
-          "Verschlusszeit": "1/80 Sek."
-      },
-      {
-          "name": "Sven Jaun",
-          "title": "Fanta",
-          "text": "Eine Fanta",
-          "ISO": "100",
-          "Brennweite": "9.2mm",
-          "Blende": "f\/3.2",
-          "Verschlusszeit": "1/40 Sek."
-      },
-      {
-          "name": "Sven Jaun",
-          "title": "Trottinett",
-          "text": "Ein Trottinett",
-          "ISO": "100",
-          "Brennweite": "8mm",
-          "Blende": "f\/3.0",
-          "Verschlusszeit": "1/100 Sek."
-      },
-      {
-          "name": "Sven Jaun",
-          "title": "Türschloss",
-          "text": "Eine Tuerschloss",
-          "ISO": "100",
-          "Brennweite": "38.5mm",
-          "Blende": "f\/3.7",
-          "Verschlusszeit": "1/60 Sek."
-      }]}},
+        onInit() {},
+        onMounted(currentState) {
+            currentState.data.photoInfos = [{
+                    "name": "Dominic Jaermann",
+                    "title": "Briefkasten",
+                    "text": "Ein Briefkasten eines einheimischen",
+                    "ISO": "100",
+                    "Brennweite": "4.8mm",
+                    "Blende": "f/3.2",
+                    "Verschlusszeit": "1/100 Sek."
+                },
+                {
+                    "name": "Dominic Jaermann",
+                    "title": "Grill",
+                    "text": "Text zu Grill",
+                    "ISO": "100",
+                    "Brennweite": "11.5mm",
+                    "Blende": "f\/3.4",
+                    "Verschlusszeit": "1/50 Sek."
+                },
+                {
+                    "name": "Dominic Jaermann",
+                    "title": "Monolith",
+                    "text": "Ein Monolith",
+                    "ISO": "100",
+                    "Brennweite": "4.8mm",
+                    "Blende": "f\/2.8",
+                    "Verschlusszeit": "1/30 Sek."
+                },
+                {
+                    "name": "Mladen Brankovic",
+                    "title": "Giraffe",
+                    "text": "Eine Giraffe",
+                    "ISO": "100",
+                    "Brennweite": "4.8mm",
+                    "Blende": "f\/2.8",
+                    "Verschlusszeit": "1/80 Sek."
+                },
+                {
+                    "name": "Mladen Brankovic",
+                    "title": "Skulptur",
+                    "text": "Eine Skulptur",
+                    "ISO": "100",
+                    "Brennweite": "4.8mm",
+                    "Blende": "f\/4.0",
+                    "Verschlusszeit": "1/200 Sek."
+                },
+                {
+                    "name": "Mladen Brankovic",
+                    "title": "Verkehrshut",
+                    "text": "Ein Verkehrshut",
+                    "ISO": "100",
+                    "Brennweite": "8.5mm",
+                    "Blende": "f\/3.2",
+                    "Verschlusszeit": "1/80 Sek."
+                },
+                {
+                    "name": "Sven Jaun",
+                    "title": "Fanta",
+                    "text": "Eine Fanta",
+                    "ISO": "100",
+                    "Brennweite": "9.2mm",
+                    "Blende": "f\/3.2",
+                    "Verschlusszeit": "1/40 Sek."
+                },
+                {
+                    "name": "Sven Jaun",
+                    "title": "Trottinett",
+                    "text": "Ein Trottinett",
+                    "ISO": "100",
+                    "Brennweite": "8mm",
+                    "Blende": "f\/3.0",
+                    "Verschlusszeit": "1/100 Sek."
+                },
+                {
+                    "name": "Sven Jaun",
+                    "title": "Türschloss",
+                    "text": "Eine Tuerschloss",
+                    "ISO": "100",
+                    "Brennweite": "38.5mm",
+                    "Blende": "f\/3.7",
+                    "Verschlusszeit": "1/60 Sek."
+                }
+            ]
+        }
+    },
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
     });
+});
+
+$(document).ready(function() {
+
+    $(".navbar-brand").click(function() {
+        $(".navbar-brand").removeClass("active");
+        $(this).addClass("active");
+    });
+});
+
+
+
+
+// init on page ready
+$(function() {
+    // check on each scroll event
+    $('#scrollable').scroll(function() {
+        // elements to be tested
+        var _elements = $('.pagesection');
+
+        // get most visible element (result)
+        var ele = findMostVisible(_elements);
+        if (ele[0]) {
+            let id = ele[0].outerHTML.slice(9, 14);
+            console.log(id)
+            $('#navigation').find('a').each(function() {
+                $(this).removeClass('active')
+            });
+            document.getElementById(id + "nav").className += " active"
+        }
+
+    });
+});
+
+
+function findMostVisible(_elements) {
+
+    // find 'scrollable' top and bottom position.
+    var wtop = $('#scrollable').scrollTop();
+    var wbottom = wtop + $('#scrollable').height();
+
+
+    var max = 0; // use to store value for testing
+    var maxEle = false; // use to store most visible element
+
+    // find percentage visible of each element
+    _elements.each(function() {
+
+        // get top and bottom position of the current element
+        var top = $(this).offset().top;
+        var bottom = top + $(this).height();
+
+        // get percentage of the current element
+        var cur = eleVisible(top, bottom, wtop, wbottom);
+
+        // if current element is more visible than previous, change maxEle and test value, max 
+        if (cur > max) {
+            max = cur;
+            maxEle = $(this);
+        }
+    });
+
+    return maxEle;
+}
+
+// find visible percentage
+function eleVisible(top, bottom, wtop, wbottom) {
+
+    var wheight = wbottom - wtop;
+
+    // both bottom and top is vissible, so 100%
+    if (top > wtop && top < wbottom && bottom > wtop && bottom < wbottom) {
+        return 100;
+    }
+
+    // only top is visible
+    if (top > wtop && top < wbottom) {
+        return 100 + (wtop - top) / wheight * 100;
+    }
+
+    // only bottom is visible
+    if (bottom > wtop && bottom < wbottom) {
+        return 100 + (bottom - wbottom) / wheight * 100;
+    }
+
+    // element is not visible
+    return 0;
+}
